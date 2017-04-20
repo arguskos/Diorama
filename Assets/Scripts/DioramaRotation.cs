7a872e7cc24560161bab84d42b421b8f29f67fa1
WouterWeynants <wouter.weynants@hotmail.com>
2017-04-20 10:36:54 +0200

    Changed light, adjusted rotationscript and prepared Diorama_001 Scene

---
+++ Assets/Scripts/DioramaRotation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DioramaRotation : MonoBehaviour {
+
+
+    public float MoveSpeed = 0.5f;
+    private GameObject _level;
+    private Vector3 _levelRotation;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Set Level to this
+        _level = this.gameObject;
+
+        //Set Level to correct position
+        var startPos = new Vector3(0,0,0);
+        _level.transform.position = startPos;
+        _levelRotation = _level.transform.rotation.eulerAngles;
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Set Amount of rotation to 45 degrees
+        var rotamount = new Vector3(0, 45, 0);
+
+
+        //On Button Up
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            _levelRotation += rotamount;
+        }
+
+        //On Button Down
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            _levelRotation -= rotamount;
+        }
+
+        //Tween Rotation of Level
+        if (_levelRotation.y >= 360 || _levelRotation.y <= -360 )
+        {
+            _levelRotation.y = 0;
+        }
+        iTween.RotateTo(_level, _levelRotation, MoveSpeed);
+    }
+}

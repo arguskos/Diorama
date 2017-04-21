7582944f8bf9108732c375a4b7b2d08c959d1210
WouterWeynants <wouter.weynants@hotmail.com>
2017-04-21 10:11:54 +0200

    Audio and sign update

--- Assets/Scripts/DioramaRotation.cs
+++ Assets/Scripts/DioramaRotation.cs
@@ -8,10 +8,14 @@ public class DioramaRotation : MonoBehaviour {
     public float MoveSpeed = 0.5f;
     private GameObject _level;
     private Vector3 _levelRotation;
+    private SoundManager _soundManager;
 
     // Use this for initialization
     void Start ()
     {
+        //Get soundmanager
+        _soundManager = GameObject.FindGameObjectWithTag("SoundManager").GetComponent<SoundManager>();
+            
         //Set Level to this
         _level = this.gameObject;
 
@@ -24,18 +28,20 @@ public class DioramaRotation : MonoBehaviour {
 	void Update () {
 
         //Set Amount of rotation to 45 degrees
-        var rotamount = new Vector3(0, 90, 0);
+        var rotamount = new Vector3(0, 45, 0);
 
 
         //On Button Up
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetButtonDown("turnL"))
         {
-            _levelRotation += rotamount;
+            _soundManager.PlaySound("SelectionInGame");
+            _levelRotation += rotamount;    
         }
 
         //On Button Down
         if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetButtonDown("turnR"))
         {
+            _soundManager.PlaySound("SelectionInGame");
             _levelRotation -= rotamount;
         }
 
